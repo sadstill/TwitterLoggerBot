91238cb3ba493e2f689d17ac67b36d2558b1cb5c
sadstill <yarik20052013@gmail.com>
2023-11-03 03:08:47 +0300

    pushed some changes

---
+++ src/main/java/dev/automation/twitter_bot/contoller/TwitterContoller.java
@@ -0,0 +1,8 @@
+package dev.automation.twitter_bot.contoller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TwitterContoller {
+
+}
---
+++ src/main/java/dev/automation/twitter_bot/service/LoggerService.java
@@ -0,0 +1,9 @@
+package dev.automation.twitter_bot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface LoggerService {
+
+
+}
